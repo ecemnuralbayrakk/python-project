13652394bebeec49fd1b4e7acbd7585232e752ae
Ecem Nur Albayrak <93910832+ecemnuralbayrakk@users.noreply.github.com>
2024-11-18 15:01:47 +0300

    Add number guessing game with logo and gameplay logic (#5)

---
+++ luck/play.py
@@ -0,0 +1,44 @@
+import random
+from art import logo
+
+
+def random_number():
+    return random.randint(1, 100)
+
+
+def return_prompt(number, predict):
+    if number > predict:
+        print(f"{predict} is too high.{number}")
+        return "Too high."
+    elif number < predict:
+        print(f"{predict} is too low.{number}")
+        return "Too low."
+    else:
+        return f"You got it! The answer was {number}"
+
+
+def play_game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    number = random_number()
+    print(number)
+
+    if difficulty == "easy":
+        for _ in range(10):
+            print(f"You have {10 - _} attempts remaining to guess the number.")
+            predict = int(input("Make a guess: "))
+            print(return_prompt(number, predict))
+            if number == predict:
+                break
+    else:
+        for _ in range(5):
+            print(f"You have {5 - _} attempts remaining to guess the number.")
+            predict = int(input("Make a guess: "))
+            print(return_prompt(number, predict))
+            if number == predict:
+                break
+
+
+play_game()
